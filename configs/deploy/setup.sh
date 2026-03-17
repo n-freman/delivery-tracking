533925f0d7f064144e56ae2f349a11396d77286d
n-freman <smiron1703@gmail.com>
2026-03-17 16:07:36 +0500

    update deploy steps

--- configs/deploy/setup.sh
+++ configs/deploy/setup.sh
@@ -8,8 +8,8 @@ apt update && apt upgrade -y
 
 # ── Install system packages ───────────────────────────────────────────────────
 apt install -y \
-    python3.11 \
-    python3.11-venv \
+    python3.12 \
+    python3.12-venv \
     python3-pip \
     python3-dev \
     postgresql \
--- src/core/settings.py
+++ src/core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6@1mowf$ua@-ijd6alb4lqb^5x@)+4ygs3cv@mdv!()3lmzw89"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") in ["True", "true", "1"]
