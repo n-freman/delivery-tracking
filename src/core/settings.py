846474812f2cf080b265bc8e1d58a56be4592cb4
n-freman <smiron1703@gmail.com>
2026-03-18 14:49:03 +0500

    remove unnecessary import

--- src/core/settings.py
+++ src/core/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from dotenv import load_dotenv
 
